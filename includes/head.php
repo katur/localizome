3f4694c230be2bcb693df12b7691dc4193ea2acd
Katherine Erickson <katherine.erickson@gmail.com>
2013-02-04 16:17:30 -0500

    initial commit, mostly empty files

---
+++ includes/head.php
@@ -0,0 +1,8 @@
+<!-- Copyright (c) 2013 Katherine Erickson -->
+
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/includes/global.php"); ?>
+<head>
+	<title>Early Embryo Localizome</title>
+	<link rel="stylesheet" type="text/css" href="/stylesheets/style.css">
+	<!-- <script type="text/javascript" src="/js/jquery.js"></script> -->
+</head>
---
+++ index.php
@@ -0,0 +1,12 @@
+<!-- Copyright (c) 2013 Katherine Erickson -->
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+
+<html>
+	<?php include("includes/head.php"); ?>
+	<body id='index'>
+		<div>
+			Hello Worms
+		</div>
+	</body>
+</html>
