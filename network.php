026c11a27e72e06168d8a890846fabcd1c667bd4
Katherine Erickson <katherine.erickson@gmail.com>
2013-02-05 17:37:36 -0500

    added network image placeholder. minor modifications.

--- network.php
+++ network.php
@@ -3,11 +3,11 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 
 <html>
-	<?php include("includes/head.php"); ?>
+	<?php include('includes/head.php'); ?>
 	<body id='index'>
 		<div id='content'>
-		    <?php include("includes/header.php"); ?>
-		    
+		    <?php include('includes/header.php'); ?>
+		    <img src='images/networkimage.jpg' class='big-image'>
 		</div>
 	</body>
 </html>
