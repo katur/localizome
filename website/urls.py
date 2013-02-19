775e6e44f6bd22830125a99d656c9b77d9cc204d
Katherine Erickson <katherine.erickson@gmail.com>
2013-02-19 17:13:34 -0500

    changed proteins page to a generic ListView template

--- website/urls.py
+++ website/urls.py
@@ -1,12 +1,18 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import DetailView, ListView
+from website.models import Protein
 
 # urlpatterns is a module-level variable.
 # it is a sequence of tuples, each in the format: 
 # (regex, Python callback fxn [,optional dictionary])
 
-urlpatterns = patterns('website.views',
-    url(r'^$', 'home', name='home'),
-    url(r'^proteins$', 'display_proteins', name='proteins'),
-    url(r'^protein/(?P<protein_common_name>.+)$', 'protein_detail'),
+urlpatterns = patterns('',
+    url(r'^$', 'website.views.home', name='home'),
+    url(r'^proteins$', 
+	    ListView.as_view(
+		    queryset = Protein.objects.order_by('common_name'),
+		    context_object_name='protein_list',
+		    template_name='protein_list.html')),
+    url(r'^protein/(?P<protein_common_name>.+)$', 'website.views.protein_detail'),
     #url(r'^localizome/', include('localizome.foo.urls')),
 )
--- website/views.py
+++ website/views.py
@@ -5,13 +5,8 @@ from django.shortcuts import render_to_response, get_object_or_404
 # Note: render_to_response() is a shortcut to load a template, pass it a context,
 # and render it. See https://docs.djangoproject.com/en/1.4/intro/tutorial03/
 
-
 def home(request):
 	return render_to_response('home.html')
-
-def display_proteins(request):
-	protein_list = Protein.objects.all().order_by('common_name')
-	return render_to_response('protein_list.html', {'protein_list': protein_list})
 
 def protein_detail(request, protein_common_name):
 	p = get_object_or_404(Protein, common_name=protein_common_name)
