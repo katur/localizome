19c1ff8ca1f12d4c39da3bfac6889c9abbc6b95c
Katherine Erickson <katherine.erickson@gmail.com>
2013-02-21 17:56:13 -0500

    styled the menu on hover / active, and added some menu items. added placeholder network image. got rid of generic views to simplify (now all are render_to_response). added more DRY urls to the templates... needed to add 'django.core.context_processors.request' to TEMPLATE_CONTEXT_PROCESSORS in settings

--- website/urls.py
+++ website/urls.py
@@ -1,16 +1,14 @@
 from django.conf.urls import patterns, include, url
-from website.views import ProteinList
 
 # urlpatterns is a module-level variable
 urlpatterns = patterns('website.views', # first arg prefix for views
 	# subsequent args tuples: regex, view, optional dictionary
 	url(r'^$', 'home', name='home_url'),
-	url(r'^proteins$', ProteinList.as_view(template_name='protein_list.html'), 
-		name='proteins_url'),
-	url(r'^protein/(?P<common_name>.+)$', 'protein_detail', 
-		name='protein_detail_url')
-	#    url(r'^protein/(?P<pk>.+)$', 
-#	    DetailView.as_view( # a generic disply view
-#		    model=Protein,
-#		    template_name='protein_detail.html')),
+	url(r'^proteins$', 'protein_list', name='proteins_url'),
+	url(r'^protein/(?P<common_name>.+)$', 'protein_detail', name='protein_detail_url')
+	
+	# below are to implement generic views ListView and DetailView.
+	# I un-implemented them to decrease confusion. 
+	# url(r'^proteins$', ProteinList.as_view(template_name='protein_list.html'), name='proteins_url'),
+	# url(r'^protein/(?P<pk>.+)$', DetailView.as_view(model=Protein, template_name='protein_detail.html')),
 )
--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
 from django.shortcuts import render_to_response, get_object_or_404
-from django.views.generic import ListView
+#from django.views.generic import ListView
 from django.template import RequestContext #extends Context; needed for STATIC_URL
 from website.models import Protein
 
@@ -8,9 +8,14 @@ from website.models import Protein
 def home(request):
 	return render_to_response('home.html', context_instance=RequestContext(request))
 
-class ProteinList(ListView):
-	model = Protein
+def protein_list(request):
+	p = Protein.objects.all()
+	return render_to_response('protein_list.html', {'proteins': p}, context_instance=RequestContext(request))
 
 def protein_detail(request, common_name):
 	p = get_object_or_404(Protein, common_name=common_name)
 	return render_to_response('protein_detail.html', {'protein': p}, context_instance=RequestContext(request))
+
+# below is to implement generic view ListView. I un-implemented it to simplify things.
+# class ProteinList(ListView):
+#	model = Protein
